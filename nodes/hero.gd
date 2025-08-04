c57d4d1efde1bff2be77e49847e29694ba81bf71
hbb <cquhbb@163.com>
2025-08-04 15:33:26 +0800

    chore: add tile images and remove unused files
    - Add new tile images to the images/tiles directory
    - Remove unused files and directories related to the Kenney platformer pack

--- nodes/hero.gd
+++ nodes/hero.gd
@@ -1,15 +1,15 @@
 extends CharacterBody2D
 
-# Horizontal movement speed
-@export var speed = 200.0
+@export var  physics_unit = 100
+# Horizontal movement move_speed
+@export var move_speed = 2.0
 
 # Jump parameters
-@export var max_jump_velocity = -400.0
-@export var min_jump_velocity = -200.0
-@export var jump_upward_acceleration = 2000.0  # Acceleration while holding jump
-@export var gravity = 1000.0
-@export var jump_gravity = 1000.0  # Gravity when not holding jump
-@export var release_gravity = 1500.0  # Gravity when releasing jump button
+# @export var max_jump_velocity = -400.0
+@export var min_jump_velocity = 2.0
+@export var jump_upward_acceleration = 20.0  # Acceleration while holding jump
+@export var jump_gravity = 10.0  # Gravity when not holding jump
+@export var release_gravity = 15.0  # Gravity when releasing jump button
 
 # Timing parameters for variable jump
 const MIN_JUMP_TIME = 0.05  # 50ms for minimum jump
@@ -26,9 +26,9 @@ func _physics_process(delta):
 		jump_key_held = true
 		jump_input_time = 0.0
 		is_jumping = true
-		
+
 		# Immediate minimal jump to avoid input lag
-		velocity.y = min_jump_velocity
+		velocity.y = -min_jump_velocity * physics_unit
 	
 	if Input.is_action_just_released("JUMP"):
 		jump_key_held = false
@@ -36,7 +36,7 @@ func _physics_process(delta):
 	# Apply jump acceleration while holding jump and within time window
 	if is_jumping and jump_key_held and jump_input_time < MAX_JUMP_TIME:
 		# Apply upward acceleration while holding jump button
-		velocity.y += jump_upward_acceleration * delta
+		velocity.y -= jump_upward_acceleration * physics_unit * delta
 		print("Jump:", jump_input_time)
 	
 	# Update jump timer
@@ -50,11 +50,11 @@ func _physics_process(delta):
 	# Apply gravity based on jump button state
 	if not is_on_floor():
 		if jump_key_held:
-			velocity.y += jump_gravity * delta
+			velocity.y += jump_gravity * physics_unit * delta
 			print("Jump hold:", jump_input_time)
 		else:
 			# print("Jump release:", jump_input_time)
-			velocity.y += release_gravity * delta
+			velocity.y += release_gravity * physics_unit * delta
 	# else:
 	# 	# Reset jump state when on floor
 	# 	is_jumping = false
@@ -62,10 +62,10 @@ func _physics_process(delta):
 	# Handle horizontal movement
 	var direction = Input.get_axis("MOVE_LEFT", "MOVE_RIGHT")
 	if direction:
-		velocity.x = direction * speed
+		velocity.x = direction * move_speed * physics_unit
 	else:
 		# Apply friction when no input
-		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.x = move_toward(velocity.x, 0, move_speed * physics_unit)
 	
 	# Apply movement
 	move_and_slide()
