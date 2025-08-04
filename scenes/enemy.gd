71586b2d958574578b0e2927f84146a6433e0696
hbb <cquhbb@163.com>
2025-08-04 18:28:33 +0800

    feat(enemy): add start_speed and adjust enemy movement parameters
    - Add start_speed variable to Enemy script for initial movement speed
    - Set initial velocity.x to start_speed in _ready() function
    - Adjust enemy's acceleration and max_speed in main.tscn for more dynamic movement

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -3,10 +3,12 @@ extends CharacterBody2D
 @export var physics_unit: float = 100
 @export var acceleration: float = 1
 @export var max_speed: float = 10
+@export var start_speed: float = 1
 
 
 func _ready():
 	add_to_group("enemy")
+	velocity.x = start_speed;
 	
 ## TODO：设置多个节点，可配置速度阶梯
 ## 简单加速
