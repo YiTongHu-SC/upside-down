cc5c884065c6e577ff5f53cb23475a26bc5cc5c6
hbb <cquhbb@163.com>
2025-08-04 22:41:15 +0800

    fix(enemy): reset enemy position and velocity
    - Update enemy reset logic to include velocity reset
    - Add game restart handling for screen color
    - Improve screen color flip logic with switch view condition

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -21,3 +21,4 @@ func _physics_process(delta):
 
 func reset():
 	position = init_pos
+	velocity.x = start_speed;
--- scenes/screen_color.gd
+++ scenes/screen_color.gd
@@ -3,8 +3,13 @@ extends ColorRect
 func _ready() -> void:
 	set_upside_down(false)
 	GameDataGlobal.on_upside_down.connect(self.set_upside_down)
+	GameDataGlobal.on_game_restart.connect(self.reset)
 
 ## 设置material的参数值，其中材质由shader定义，定义了bool upside_down
 func set_upside_down(value: bool):
+	if not GameDataGlobal.switch_view: return # 不翻转
 	if material is ShaderMaterial:
 		material.set_shader_parameter("upside_down", value)
+
+func reset():
+	material.set_shader_parameter("upside_down", false)
